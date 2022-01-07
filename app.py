ae0c3d1cadef9468421feb51d4e84147add9cb2e
IronelisHerrera <ironelisherrerav@gmail.com>
2022-01-07 08:30:22 -0400

    model version 3 stable

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from helpers.functions import read_and_prep_images, read_and_prep_image_from_url
 app = Flask(__name__)
 
 
-PATH_MODEL = './models/jan_model_trainingv4.h5'
+PATH_MODEL = './models/jan_model_trainingv3.h5'
 # CLASSES = ['cellphone', 'digitalwatch', 'headphone',
 #            'laptop', 'speaker', 'tablet', 'television'] #v2
 CLASSES = ['cellphone', 'digitalwatch', 'headphone',
